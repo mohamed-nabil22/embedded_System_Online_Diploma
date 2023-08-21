b2c2ce3a8e077bb3fd462b4b77bf4e2c34f76d76
mohamed-nabil22 <nmohamednabil5@gmail.com>
2023-08-21 20:17:09 +0300

    Added the questions

--- Assignments_C/Assignment-1/Example6&7/main.c
+++ Assignments_C/Assignment-1/Example6&7/main.c
@@ -4,7 +4,26 @@
  *  Created on: Aug 21, 2023
  *      Author: Mohamed Shalaby
  */
+/*########################################################################
 
+EX6:
+
+Write Source Code to Swap Two Numbers
+
+#########Console_output######
+Enter value of a: 1.20
+Enter value of b: 2.45
+
+After swapping, value of a = 2.45
+After swapping, value of b = 1.2
+#############################
+########################################################################
+
+EX7:
+it is an interview trick
+
+Write Source Code to Swap Two Numbers without temp variable.
+*/
 #include <stdio.h>
 void swap_with_temp();
 void swap_without_temp();
