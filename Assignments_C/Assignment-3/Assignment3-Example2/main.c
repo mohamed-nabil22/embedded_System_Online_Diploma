2bb12bb8b8a5d16f96594fbcec3beac7a0f097d9
mohamed-nabil22 <nmohamednabil5@gmail.com>
2023-08-28 00:24:05 +0300

    Add Example2

---
+++ Assignments_C/Assignment-3/Assignment3-Example2/main.c
@@ -0,0 +1,36 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 27, 2023
+ *      Author: Mohamed Shalaby
+ */
+//c program to calculate average using arrays
+#include<stdio.h>
+
+void main()
+{
+	int number;
+	printf("Enter the number of data:");
+	fflush(stdout);
+	scanf("%d",&number);
+	float array[100];
+	float sum_of_array=0;
+	int count;
+	while(number>100||number<=0)
+		{
+		printf("Error number should be from 1 to 100");
+		printf("please Enter the Number again:");
+		fflush(stdout);
+		scanf("%d",&number);
+		}
+	for(int i=0 ; i < number;i++)
+	{
+		printf("%d. Enter number: ",i+1);
+		fflush(stdout);
+		scanf("%f",&array[i]);
+		sum_of_array += array[i];
+	}
+			printf("Average=%.2f",sum_of_array/number);
+
+}
+
